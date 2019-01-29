6eea482f2670c37de93d1b8ebc29e5016f7afb4a
LLT <1564940789@qq.com>
2019-01-29 14:30:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 cp ./sources.list /etc/apt -f
 cp ./unlock /data
 apt update
-apt install gcc
-apt install make
+apt install gcc -Y
+apt install make -Y
 /usr/share/rbenv/versions/2.4.4/bin/gem install byebug
+/usr/share/rbenv/versions/2.5.3/bin/gem install byebug
